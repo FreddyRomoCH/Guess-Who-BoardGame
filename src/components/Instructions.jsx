55333a3aa61bc28f834f1e5f0d7316a759047a02
Freddy Romo <freddyromochavez@gmail.com>
2024-08-13 00:04:12 +0300

    Fixing modal height

--- src/components/Instructions.jsx
+++ src/components/Instructions.jsx
@@ -1,6 +1,6 @@
 export function Instructions({ handleIntructionsClose }) {
   return (
-    <div className="bg-instructions absolute flex flex-col justify-center items-center z-50 bg-black/85 top-0 left-0 w-full h-full overflow-hidden">
+    <div className="bg-instructions fixed flex flex-col justify-center items-center z-50 bg-black/85 top-0 left-0 w-full h-full overflow-hidden">
       <div className="relative bg-[#850E16] p-4 rounded-lg flex flex-col justify-between items-center w-10/12 md:w-1/2">
         <div
           className="close absolute w-16 h-16 -top-8 -right-8 cursor-pointer"
